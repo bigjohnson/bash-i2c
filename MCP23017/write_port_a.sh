83ac4a81c329be8d4a34b09fb83fa487a414f2fd
Alberto Panu <alberto@panu.it>
2018-03-15 21:09:26 +0100

    Update write_port_a.sh

--- MCP23017/write_port_a.sh
+++ MCP23017/write_port_a.sh
@@ -3,8 +3,14 @@ I2CPORT=1
 MCP23017=0x20
 IODIR=0x00
 OLAT=0x14
+
+# MCP23017 setup
 i2cset -y ${I2CPORT} ${MCP23017} ${IODIR} 0x00
-trap "{ i2cset -y ${I2CPORT} ${MCP23017} ${OLAT} 0; }" EXIT
+i2cset -y ${I2CPORT} ${MCP23017} ${OLAT} 0x00
+
+# Power off all led on exit...
+trap "{ i2cset -y ${I2CPORT} ${MCP23017} ${OLAT} 0x00; }" EXIT
+
 while true
 do
         for LED in 1 2 4 8 16 32 64 128
