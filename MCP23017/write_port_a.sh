7dc354c0d521bf3b2d8225bdcfe2afd34b8ae18b
Alberto Panu <alberto@panu.it>
2018-03-15 21:04:31 +0100

    Create write_port_a.sh

---
+++ MCP23017/write_port_a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+I2CPORT=1
+MCP23017=0x20
+IODIR=0x00
+OLAT=0x14
+i2cset -y ${I2CPORT} ${MCP23017} ${IODIR} 0x00
+trap "{ i2cset -y ${I2CPORT} ${MCP23017} ${OLAT} 0; }" EXIT
+while true
+do
+        for LED in 1 2 4 8 16 32 64 128
+        do
+        i2cset -y ${I2CPORT} ${MCP23017} ${OLAT} ${LED}
+        done
+done
