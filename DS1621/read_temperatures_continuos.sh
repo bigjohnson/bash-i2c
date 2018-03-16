8d9c9df21b77148ac00c64db895d83ea15e1b1c3
Alberto Panu <alberto@panu.it>
2018-03-16 11:55:25 +0100

    Update read_temperatures_continuos.sh

--- DS1621/read_temperatures_continuos.sh
+++ DS1621/read_temperatures_continuos.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# read delay in seconds
 RITARDO=1
 twos() { x=$((16#$1)); [ "$x" -gt 127 ] && ((x=x-256)); echo "$x"; }
+# set up continuous measurement
 i2cset -y 1 0x48 0xac 0x00
+# start measurements
 i2cset -y 1 0x48 0xee
 while true
 do
+        read the temperature
         DATA=$(i2cget -y 1 0x48 0xAA w)
         if [ "${DATA:2:2}" = "80" ]
         then
